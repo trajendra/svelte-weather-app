325aa8c42555166a8ed4f2625b8d97a7a7a5a6cc
Raj <mailbox.rajj@gmail.com>
2020-05-31 03:50:35 +0000

    Initial commit with full source code

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import SearchCards from "./components/Search.svelte";
+	
+	export let name;
+	
+</script>
+<main>
+	<h1>Welcome to {name}!</h1>
+
+	<SearchCards />
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Search.svelte
@@ -0,0 +1,69 @@
+<script>
+	import withQuery from "with-query"
+	import Card from "./Card.svelte";
+	import Button from "./UI/Button.svelte";
+	import Inputbar from "./UI/Inputbar.svelte";
+	export let result = "";
+	export let formData = {
+    	city: "",
+  	};
+  
+  	let searchitems = [];
+		
+	async function handleSubmit(e) {
+		e.preventDefault();
+		await fetch(withQuery('http://api.openweathermap.org/data/2.5/weather', {
+			q: formData.city,
+			units: 'imperial',
+			appid: '271d1234d3f497eed5b1d80a07b3fcd1',
+		}))
+		.then(resp => resp.json())
+		.then(data => (result = data));
+
+		const newSearchitem = {
+			id: Math.random().toString(),
+			temp: result["main"]["temp"],
+			icon: result["weather"][0]["icon"],
+			...formData
+		};
+
+		searchitems = [...searchitems, newSearchitem];
+
+	}
+
+	function formHandler(event) {
+		formData[event.target.name] = event.target.value;	
+	}
+
+</script>
+<main>
+
+	<form on:submit={handleSubmit}>
+		<Inputbar bind:value={formData.city} on:input={formHandler} />
+		<Button type="submit" caption="Search" />
+	</form>
+	<section id="searchitems">
+		{#each searchitems as searchitem}
+			<Card city={searchitem.city} temp={searchitem.temp} icon={searchitem.icon} />
+		{/each}
+	</section>	
+
+</main>
+
+
+<style>
+
+ 
+  section {
+ 	
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(6, 1fr);
+    }
+  }
+</style>
